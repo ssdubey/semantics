c46d574364888bc0a731aa615f5cc992e9c0e401
shashank shekhar dubey <shashank.shekhardubey@gmail.com>
2020-12-27 22:45:40 +0530

    2

--- banyan/banyan.v
+++ banyan/banyan.v
@@ -49,31 +49,13 @@ Inductive cmd :=
 |Assign : var -> value -> cmd
 |Assign_bs : hash -> value -> cmd
 |Assign_ts : branch -> hash -> cmd
+|Read_ts : branch -> tagstore -> cmd
 |Skip: cmd.
-
-
-(*Inductive banyan_cmd :=
-|returnvalue : blockstore -> banyan_cmd
-|Assign_bs : hash -> value -> blockstore -> banyan_cmd.*)
 
 Definition returnvalue : Type := (blockstore * tagstore).
 
 Delimit Scope cmd_scope with cmd.
 Infix ";;" := Seq (at level 80) : cmd_scope.
-
-(*Inductive dummy_cmd_step : (blockstore * hash * value) -> (blockstore * hash * value) -> Prop :=
-|cmd_step_Assignbs : forall bs h v,
-  dummy_cmd_step (bs, h, v) (bs $+ (h, v), h, v).
-
-Definition dummy_banyan_add (br: branch) (k: string) (v: string) (bs: blockstore) :=
-  Assign_bs bs (create_block_hash v) (Value_block v).
-
-Example ex: exists st, dummy_cmd_step ($0, (create_block_hash "val"), (Value_block "val")) st.
-Proof.
-eexists.
-econstructor.
-Qed.
-*)
 
 Definition banyan_add (br: branch) (k: string) (v: string) :=
 (*Seq (Assign_bs (create_block_hash v) (Value_block v)) (Assign_bs (create_block_hash v) (Value_block v)).
@@ -90,35 +72,33 @@ Definition banyan_add (br: branch) (k: string) (v: string) :=
   Assign_bs commithash (Value_commit c);;
   
   Assign_ts br commithash
+  (*Assign_ts br h*)
 
   )%cmd.
 
 
-
-(*Definition banyan_add (br: branch) (k: string) (v: string) (bs: blockstore) (ts: tagstore): returnvalue := 
-  let bs := Assign_bs (create_block_hash v) (Value_block v) bs in
-  let t := (Content, k, (create_block_hash v))::[] in
-  let bs := Assign_bs (create_tree_hash t) (Value_tree t) bs in (bs, ts).*)
-
 (*as of now it is doing main operations*)
 Inductive cmd_step : cmd * blockstore * tagstore -> cmd * blockstore * tagstore -> Prop :=
 |Assign_bs_Step : forall blockhash blockvalue bs ts,
-  cmd_step ((Assign_bs blockhash blockvalue), bs, ts) (Skip, (bs $+ (blockhash, blockvalue)), ts)
+  cmd_step (Assign_bs blockhash blockvalue, bs, ts) (Skip, (bs $+ (blockhash, blockvalue)), ts)
 |Assign_ts_Step : forall br h bs ts,
   cmd_step ((Assign_ts br h), bs, ts) (Skip, bs, (ts $+ (br, h))).
+(*|Cmd_Skip : forall bs ts,
+  cmd_step (Skip, bs, ts) (Skip, bs, ts).*)
+(*|Read_ts_Step: 
+  cmd_step (Read_ts *)
 
 (*as of now it is resolving the operations to perform main operations in cmd_step*)
 Inductive basic_step : (cmd * blockstore * tagstore * cmd) -> (cmd * blockstore * tagstore * cmd) -> Prop :=
-|SeqStep : forall c1 c2 k bs ts c1' bs' ts',
+|SeqStep : forall c1 c2 k bs ts,
+  basic_step (Seq c1 c2, bs, ts, k) (c1, bs, ts, (Seq c2 k))
+|SeqSolve : forall c1 bs ts c1' bs' ts' k,
   cmd_step (c1, bs, ts) (c1', bs', ts') ->
-  basic_step (Seq c1 c2, bs, ts, k) (c1', bs', ts', (Seq c2 k))
+  basic_step (c1, bs, ts, k) (c1', bs', ts', k)
 |SeqStep2 : forall bs ts c k,
   basic_step (Skip, bs, ts, (Seq c k)) (c, bs, ts, k).
-
-(*Definition checkblock (st: cmd * blockstore * tagstore) :=
-match st with
-|(_, bs, ts) => (bs $? (create_block_hash "value")) = Some (Value_block "value")
-end.*)
+(*|FinalStep : forall bs ts,
+  basic_step (Skip, bs, ts, Skip) (Skip, bs, ts, Skip).*)
 
 
 (*writing basic_step (...) means it will take one step and whatever is the output, it will be assigned to st.
@@ -129,40 +109,49 @@ Proof.
 eexists.
 propositional.
 unfold banyan_add.
+
 eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_ts_Step.
 
 econstructor.
-econstructor.
-econstructor.
-
-(*I couldnot find the order in which constructors are applied, even though it is all passing with econstructor.*)
 
 Qed.
 
-(*let h := create_block_hash v in
-  Assign_bs h (Value_block v) ;;
-
-  let t := (Content, k, h)::[] in
-  let treehash := create_tree_hash t in
-  Assign_bs treehash (Value_tree t);;
-
-  let c := ([], treehash) in 
-  let commithash := create_commit_hash c in 
-  Assign_bs commithash (Value_commit c);;
-  
-  Assign_ts br commithash
------------------
-*)
 
 Definition searchKey value_tree :=  (*accomodate key k in search*)
 match value_tree with
 | Value_tree t => 
   match t with
-  | [] => create_block_hash ""
-  | [(_, _, h)] => h
-  | _ => create_block_hash ""
+  | [] => true (*create_block_hash ""*)
+  | [(_, _, h)] => true (*h*)
+  | _ => true (*create_block_hash ""*)
   end
-| _ => create_block_hash ""
+| _ => true (*create_block_hash ""*)
 end.
   
 
@@ -193,22 +182,38 @@ match opt_val with
 |None => Value_tree dummy_tree
 end.
 
-(*Definition resolve opt_commit_value :=
-match opt_value with 
-| Some x => x
-| None => create_commit_hash ([], 
+(*Inductive read_semantics : (cmd * option hash) -> (cmd * option hash) -> Prop :=
+|Read_ts_step : forall br ts,
+  read_semantics (Read_ts br ts, None) (Skip, ts $? br).*)
+
+Definition read_ts (br: branch) (ts: tagstore) :=
+match (ts $? br) with
+|Some x => x
+|None => Dummy_hash
 end.
 
-Definition resolve opt_tree_value :=
-match opt_value with 
-| Some x => x
-| None => create_tree_hash []
-end.*)
+Definition read_bs (hashArg: hash) (bs: blockstore) :=
+match (bs $? hashArg) with
+|Some x => x
+|None => Dummy_value
+end.
 
-Definition verify_store_helper (st: cmd * blockstore * tagstore * cmd)  br (v:string) :=
+Definition read_bs_tree (hashArg : hash) (bs: blockstore) :=
+match (bs $? hashArg) with
+|Some x => x
+|None => Dummy_value
+end.
+
+Definition verify_store_helper (st: cmd * blockstore * tagstore * cmd)  br (v:block) :=
 match st with
 |(_, bs, ts, _) =>
-  let latestCommitHash := resolve_opt_hash (ts $? br) in
+  let commitHash := read_ts br ts in
+  let value_commit :=  read_bs commitHash bs in
+  let treeHash := getSecondInCommit value_commit in
+  let value_tree := read_bs_tree treeHash bs in 
+  let blockHash := searchKey value_tree in
+blockHash
+  (*let latestCommitHash := resolve_opt_hash (ts $? br) in
   let value_commit :=  resolve_opt_commit (bs $? latestCommitHash) in
   let treeHash := getSecondInCommit value_commit in
   let value_tree := resolve_opt_tree (bs $? treeHash) in
@@ -223,7 +228,7 @@ match st with
     end
   | _ => false
   end
-
+*)
 end.
 
 
@@ -236,39 +241,257 @@ end.
 
 (*adding and checkinng the first item in the database*)
 Example ex2: exists st, (basic_step^* ((banyan_add (Branch "branch") "key" "value"), $0, $0, Skip) st) /\ 
-                                                          ((verify_store st (Branch "branch") "value") = true).
+                                                          ((verify_store_helper st (Branch "branch") "value") = true).
 Proof.
 eexists.
 propositional.
 unfold banyan_add.
+
 eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_ts_Step.
 
 econstructor.
-simplify. 
-econstructor.
+
+unfold verify_store.
+unfold verify_store_helper.
+unfold read_ts.
 simplify.
-econstructor.
+unfold read_bs.
+simplify.
+
 simplify.
 simplify.
 
-discriminate.
-equality.
+unfold read_bs_tree.
+simplify.
+
+unfold searchKey.
+
+simplify.
+Admitted.
+(*econstructor.
+
+Qed.*)
+
+
+Definition banyan_add2 (br: branch) (k1: string) (v1: string) (k2: string) (v2: string):=
+(let h := create_block_hash v1 in
+  Assign_bs h (Value_block v1) ;;
+
+  let t := (Content, k1, h)::[] in
+  let treehash := create_tree_hash t in
+  Assign_bs treehash (Value_tree t);;
+
+  let c := ([], treehash) in 
+  let commithash := create_commit_hash c in 
+  Assign_bs commithash (Value_commit c);;
+  
+  Assign_ts br commithash;;
+  
+
+
+  let h2 := create_block_hash v2 in
+  Assign_bs h2 (Value_block v2) ;;
+
+  let t2 := (Content, k2, h2)::t in
+  let treehash2 := create_tree_hash t2 in
+  Assign_bs treehash2 (Value_tree t2);;
+
+  let c2 := ([commithash], treehash2) in 
+  let commithash2 := create_commit_hash c2 in 
+  Assign_bs commithash2 (Value_commit c2);;
+  
+  Assign_ts br commithash2
+
+  )%cmd.
+
+
+
+Example ex3: exists st, basic_step^* ((banyan_add2 (Branch "branch") "key1" "value1" "key2" "value2"), 
+            $0, $0, Skip) st.
+Proof.
+eexists.
+propositional.
+unfold banyan_add2.
+
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_ts_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
+
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_ts_Step.
+
+
+econstructor.  (*not sure how its solving everything*)
 Qed.
 
 
+Definition read_store_helper (st: cmd * blockstore * tagstore * cmd)  br (v:block) :=
+match st with
+|(_, bs, ts, _) =>
+  let latestCommitHash := resolve_opt_hash (ts $? br) in
+  let value_commit :=  resolve_opt_commit (bs $? latestCommitHash) in
+  let treeHash := getSecondInCommit value_commit in
+  let value_tree := resolve_opt_tree (bs $? treeHash) in
+  let blockHash := searchKey value_tree in  (*accomodate key k in search*)
+  let value_block := (bs $? blockHash) in
+
+  match value_block with  (*value_block is of type option value*)
+  | Some x => 
+    match x with
+    | Value_block v => True
+    | _ => False
+    end
+  | _ => False
+  end
+
+end.
 
 
+Definition read_store (st: cmd * blockstore * tagstore * cmd) br v := 
+match st with
+|(_,bs,ts,_) => read_store_helper st br v
+end.
 
+Example ex4: exists st, (basic_step^* ((banyan_add (Branch "branch") "key" "value"), $0, $0, Skip) st) /\ 
+                                                          ((read_store_helper st (Branch "branch") "value")).
+Proof.
+eexists.
+propositional.
+unfold banyan_add.
 
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
 
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
 
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqStep.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_bs_Step.
 
+eapply TrcFront.
+eapply SeqStep2.
+eapply TrcFront.
+eapply SeqSolve.
+eapply Assign_ts_Step.
 
+econstructor.
+simplify.
 
+(*
+unfold read_store.
+unfold read_store_helper.
+simplify.*)
+econstructor.
 
+unfold verify_store.
+unfold verify_store_helper.
 
+econstructor.
+simplify.
+equality.
 
+Admitted.
 
+Definition isTrue (b:bool) :=
+match b with
+|true => True
+|false => False
+end.
+
+Example ex5: isTrue(true)=False.
+Proof.
+simplify.
+equality.
 
 
 
